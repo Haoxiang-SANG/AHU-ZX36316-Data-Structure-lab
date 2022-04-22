92e312e09fd2eb243c5faf29bfde2775ef9fc848
桑浩翔 <97347493+Haoxiang-SANG@users.noreply.github.com>
2022-04-22 11:46:20 +0800

    Update MidExpression.cpp

--- lab2/MidExpression.cpp
+++ lab2/MidExpression.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 char s[N];
 
-int pri(char s) {
+int pri(char s) {								// 定义优先级，+- 为 1，*/ 为 2，而其他的无所谓。
 	if (s == '+' || s == '-') return 1;
 	if (s == '*' || s == '/') return 2;
 	if (s == '(') return -1;
@@ -13,7 +13,7 @@ int pri(char s) {
 	return 100;
 }
 
-struct stackopt{
+struct stackopt{								// 操作符的栈
 	char s[N];
 	int tp;
 	void init() {
@@ -33,7 +33,7 @@ struct stackopt{
 	}
 } ops;
 
-struct stackint{
+struct stackint{								// 操作数的栈
 	double s[N];
 	int tp;
 	void init() {
@@ -50,11 +50,11 @@ struct stackint{
 	}
 } nus;
 
-void work() {
+void work() {									// 进行一次运算
 	double x2 = nus.top(); nus.pop(); 
 	double x1 = nus.top(); nus.pop();
 	char s = ops.top(); ops.pop();
-	printf("%c ", s);
+	printf("%c ", s);							// 这里是输出运算符，因为要输出后缀表达式
 	if (s == '+') nus.push(x1 + x2);
 	else if (s == '-') nus.push(x1 - x2);
 	else if (s == '/') nus.push(x1 / x2);
@@ -65,34 +65,34 @@ int main() {
 	while (scanf("%s", s + 1) == 1) {
 		ops.init(); nus.init();
 		for (ri i = 1, l = strlen(s + 1), ret = -1; i <= l; i++) {
-			if (s[i] >= '0' && s[i] <= '9') {
+			if (s[i] >= '0' && s[i] <= '9') {	// 遇到数字读入，我这样写是为了支持 2 位及以上的数字
 				if (ret == -1) ret = 0;
 				ret = ret * 10 + s[i] - '0';
 			}
 			else {
-				if (ret != -1) {
+				if (ret != -1) {				// 遇到数字，进栈，并且直接输出
 					nus.push(ret);
 					printf("%d ", ret);
 					ret = -1;
 				}
-				if (s[i] == '#' && i != 1) {
+				if (s[i] == '#' && i != 1) {	// 遇到最后一个 #，弹栈直到第一个 #
 					while (ops.top() != '#') work();
 					ops.pop();
 				}
-				else if (s[i] == ')') {
+				else if (s[i] == ')') {			// 遇到 )，弹栈到第一个左括号
 					while (ops.top() != '(') work();
 					ops.pop();
 				}
-				else if (s[i] == '(' || s[i] == '#' && i == 1) {
+				else if (s[i] == '(' || s[i] == '#' && i == 1) {	// 遇到第一个 # 或者 (，无条件进栈
 					ops.push(s[i]);
 				}
 				else {
-					while (!ops.empty() && pri(ops.top()) >= pri(s[i])) work();
+					while (!ops.empty() && pri(ops.top()) >= pri(s[i])) work();		// 遇到操作符，维护操作符栈的单调性。
 					ops.push(s[i]);
 				}
 			}
 		}
 		puts("");
-		printf("%.5lf\n", nus.top());
+		printf("%.5lf\n", nus.top());			// 最后的结果就是数字栈的唯一一个元素
 	}
 }
