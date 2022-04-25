c756b7972f3bf43145b65316ec511312d24a8f74
桑浩翔 <97347493+Haoxiang-SANG@users.noreply.github.com>
2022-04-25 21:17:40 +0800

    Update lab2Expression-zrc.cpp

--- friends/lab2Expression-zrc.cpp
+++ friends/lab2Expression-zrc.cpp
@@ -1,40 +1,39 @@
-   #include<stdio.h>
+#include<stdio.h>
 #include<stdlib.h>
 #define maxsize 100
 typedef struct
 {
-	char *base;
-	char *top;
-	int stacksize;
+    char *base;
+    char *top;
+    int stacksize;
 }sqstack;
 
-void initstack(sqstack s)//栈的初始化 
+void initstack(sqstack &s)//栈的初始化 
 {
-	s.base=(char*)malloc(maxsize*sizeof(char));
-	if(!s.base)printf("分配失败");
-	s.top=s.base;
-	s.stacksize=maxsize;
+    s.base=(char*)malloc(maxsize*sizeof(char));
+    if(!s.base)printf("分配失败");
+    s.top=s.base;
+    s.stacksize=maxsize;
 }
-void push(sqstack s,char e)//入栈 
+void push(sqstack &s,char e)//入栈 
 {
-	if(s.top-s.base==s.stacksize)printf("error");
-	*s.top++=e;
+    if(s.top-s.base==s.stacksize)printf("error");
+    *s.top++=e;
 }
-void pop(sqstack s,char &e)//出栈 
+void pop(sqstack &s,char &e)//出栈 
 {
-	if(s.top==s.base)printf("error");
-	e=*--s.top;
-	s.top=NULL;
+    if(s.top==s.base)printf("error");
+    e=*--s.top;
 }
 char gettop(sqstack s)//取栈顶元素 
 {
-	char e;
-	e=*s.top;
-	return e;
+    char e;
+    e=*(s.top - 1);
+    return e;
 }
 int in(char ch)//判断是否为运算符 
 {
-	 switch(ch)
+     switch(ch)
     {
         case '+':
         case '-':
@@ -46,7 +45,7 @@ int in(char ch)//判断是否为运算符
         default:return 0;
     }
 }
-char precede(char ch,char c)//ch是栈顶字符,c 是表达式字符
+char precede(char ch,char c)//是栈顶字符,c 是表达式字符
 {
     switch(ch)
     {
@@ -115,66 +114,69 @@ char precede(char ch,char c)//ch是栈顶字符,c 是表达式字符
 
 int operate(char a,char theta,char b)
 {
-	switch(theta)
-	{
-		case '+':return (a+b);
-		case '-':return(a-b);
-	    case '*':return(a*b);
+    switch(theta)
+    {
+        case '+':return (a+b);
+        case '-':return(a-b);
+        case '*':return(a*b);
         case '/':return(a/b);
-		
-	}
+        
+    }
 }
-
-void output(sqstack s)
-{
-	while(s.top!=s.base)
-	{
-		printf("%c",*s.base++);
-	}
- } 
 
 
 
 
 int main()
 {
-	char a[maxsize];
-	sqstack opnd,optr,queue;
-	initstack(opnd);
-	initstack(optr);
-	initstack(queue);
-	printf("输入字符串a: ");
+    char a[maxsize];
+    char d[maxsize] ;
+    sqstack opnd,optr;
+    initstack(opnd);
+    initstack(optr);
+    printf("输入字符串a: ");
     scanf("%s",a); 
-    int i=0;
+    printf("%s\n", a);
+    int i=0,h=0;
     char theta,b,c,x;
     push(optr,'#');
     while(a[i]!='#'||gettop(optr)!='#')
     {
-    	if(!in(a[i]))
-		{
-    		push(opnd,a[i]);
-    		push(queue,a[i]);
-    		i++;
-		}
-		else
-		switch(precede(gettop(optr),a[i]))
-		{
-			case'<':
-				push(optr,a[i]);i++;
-				break;
-			case'>':
-				pop(optr,theta);push(queue,theta);
-				pop(opnd,b);pop(opnd,c);
-				push(opnd,operate(c,theta,b));
-				break;
-			case'=':
-				pop(optr,x);
-				i++;
-				break;
-		}
-	}
-	i=gettop(opnd);
-	output(queue);
-	printf("\n%d",i);
-	
+        if(!in(a[i]))
+        {
+            push(opnd,a[i] - '0');
+            d[h]=a[i];
+            i++;
+            h++;
+        }
+        else
+        switch(precede(gettop(optr),a[i]))
+        {
+            case'<':
+                push(optr,a[i]);
+                i++;
+                break;
+            case'>':
+                pop(optr,theta);
+                d[h]=theta;
+                h++;
+                pop(opnd,b);pop(opnd,c);
+                push(opnd,operate(c,theta,b));
+                break;
+            case'=': 
+                if (gettop(optr) == '#' && a[i] == '#') puts("sb");
+                pop(optr,x);
+                i++;
+                break;
+        }
+    }
+    printf("后缀表达式为：");
+    for(i=0;i<=h;i++)
+    {
+        printf("%c",d[i]);
+    }
+    printf("最终值为：\n");
+    i=gettop(opnd);
+    printf("%d",i);
+    
 }
